35fab47796c017d4793672d3814ebd0bd62fe184
Xaroth Brook <devnull@xaroth.nl>
2013-12-26 00:43:59 +0100

    -Add: Initial concept to a packet base.
    (This kinda looks like Django's models, doesn't it? .. that's because
    it's inspired by it...)

---
+++ src/libopenttd/packets/base.py
@@ -0,0 +1,103 @@
+#
+# Some parts (well, a quite a lot actually) is inspired by Django's ModelBase.
+#
+import copy
+from libopenttd.util import six
+
+class PacketOptions(object):
+    def __init__(self, meta):
+        self.meta = meta
+        self.fields = []
+
+    def contribute_to_class(self, cls, name):
+        cls._meta = self
+        self.packet = cls
+        self.pid = cls.pid
+
+    def add_field(self, field):
+        self.fields.append(field)
+
+class PacketBase(type):
+    def __new__(mcs, name, bases, attrs):
+        print "__NEW__"
+        print mcs, name, bases, attrs
+        super_new = super(PacketBase, mcs).__new__
+
+        # six.with_metaclass() inserts an extra class called 'NewBase' in the
+        # inheritance tree: Packet -> NewBase -> object. But the initialization
+        # should be executed only once for a given model class.
+
+        # attrs will never be empty for classes declared in the standard way
+        # (ie. with the `class` keyword). This is quite robust.
+        if name == 'NewBase' and attrs == {}:
+            return super_new(mcs, name, bases, attrs)
+
+        # Also ensure initialization is only performed for subclasses of Packet
+        # (excluding Packet class itself).
+        parents = [base for base in bases if isinstance(base, PacketBase) and
+                    not (base.__name__ == 'newBase' and base.__mro__ == (base, object))]
+        if not parents:
+            return super_new(mcs, name, bases, attrs)
+
+        module = attrs.pop('__module__')
+        new_class = super_new(mcs, name, bases, {'__module__': module})
+        pid = attrs.pop('pid', getattr(new_class, 'pid', -1)) # Todo: Add exception when no PID is set.
+        new_class.add_to_class('pid', pid)
+        attr_meta = attrs.pop('Meta', None)
+        if not attr_meta:
+            meta = getattr(new_class, 'Meta', None)
+        else:
+            meta = attr_meta
+        new_class.add_to_class('_meta', PacketOptions(meta))
+
+        # Add all attributes to the class.
+        for obj_name, obj in attrs.items():
+            new_class.add_to_class(obj_name, obj)
+
+        field_names = set(f.name for f in new_class._meta.fields)
+
+        for base in parents:
+            #original_base = base
+            if not hasattr(base, '_meta'):
+                continue
+            parent_fields = base._meta.fields
+            for field in parent_fields:
+                if field.name not in field_names:
+                    # Add non-overlapping fields to our registry
+                    new_class.add_to_class(field.name, copy.deepcopy(field))
+
+        return new_class
+    
+    def add_to_class(cls, name, value):
+        if hasattr(value, 'contribute_to_class'):
+            value.contribute_to_class(cls, name)
+        else:
+            setattr(cls, name, value)
+
+class Packet(six.with_metaclass(PacketBase)):
+    pid = -1
+
+    def __repr__(self):
+        try:
+            utf = six.text_type(self)
+        except (UnicodeEncodeError, UnicodeDecodeError):
+            utf = '[Bad Unicode data]'
+        return '<%s (%d): %s>'  % (self.__class__.__name__, self.pid, utf)
+
+    def __str__(self):
+        if six.PY2 and hasattr(self, '__unicode__'):
+            return six.text_type(self).encode('utf-8')
+        return '%s packet' % self.__class__.__name__
+
+class PacketFieldBase(object):
+    def __init__(self, name = None, is_struct_field = False):
+        self.name = name
+        self.is_struct_field = is_struct_field
+
+    def set_attributes_from_name(self, name):
+        if not self.name:
+            self.name = name
+
+    def contribute_to_class(self, cls, name):
+        self.set_attributes_from_name(name)
+        cls._meta.add_field(self)
