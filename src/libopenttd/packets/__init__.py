a86d5ac682be1ad0ad1a74aace34aa19aee9e02b
Xaroth Brook <devnull@xaroth.nl>
2013-12-26 13:28:49 +0100

    -Add: the struct fields to __init__.py.

--- src/libopenttd/packets/__init__.py
+++ src/libopenttd/packets/__init__.py
@@ -1,5 +1,7 @@
 from .base import PacketBase, FieldBase
 from .enums import Protocol, Direction
-from .fields import Field, StructField
+from .fields import Field, StructField, CharField, BooleanField, UByteField, ByteField, UInt8Field, \
+    SByteField, Int8Field, UShortField, UInt16Field, SShortField, Int16Field, UIntField, ULongField, \
+    UInt32Field, SIntField, LongField, Int32Field, ULongLongField, UInt64Field, SLongLongField, Int64Field
 from .packet import Packet
 from .registry import registry
