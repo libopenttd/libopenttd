30868b6b22cee68c49f85743f020647a81136781
Xaroth Brook <devnull@xaroth.nl>
2013-12-26 13:09:21 +0100

    -Add: some initial logic for Struct Fields.

--- src/libopenttd/packets/fields.py
+++ src/libopenttd/packets/fields.py
@@ -1,6 +1,8 @@
 from .base import FieldBase
 from libopenttd.utils import six
 
+from struct import Struct
+
 class Field(FieldBase):
     def __init__(self, name = None, ordering = -1, *args, **kwargs):
         super(Field, self).__init__(name = name, ordering = ordering, *args, **kwargs)
@@ -11,6 +13,15 @@ class Field(FieldBase):
 
     def merge(self, other):
         self.neighbours.append(other)
+
+    def from_python(self, data):
+        raise NotImplementedError()
+
+    def to_python(self, data, index):
+        raise NotImplementedError()
+
+    def _prepare(self):
+        pass
 
     def __repr__(self):
         try:
@@ -27,5 +38,38 @@ class Field(FieldBase):
 class StructField(Field):
     struct_type = None
 
+    _structCache = {}
+
+    @classmethod
+    def get_struct(cls, fmt):
+        item = StructField._structCache.get(fmt, None)
+        if item is None:
+            item = StructField._structCache[fmt] = StructField._build_struct(fmt)
+        return item
+
+    @classmethod
+    def _build_struct(cls, fmt):
+        # We enforce the < prefix
+        if fmt[0] in ['@', '=', '<', '>', '!']:
+            fmt = fmt[1:]
+        fmt = '<%s' % fmt
+        return Struct(fmt)
+
+    def __init__(self, *args, **kwargs):
+        super(StructField, self).__init__(*args, **kwargs)
+        self.struct = None
+
     def can_merge(self, other):
         return isinstance(other, StructField)
+
+    def _prepare(self):
+        fmt = self.struct_type
+        for neigh in self.neighbours:
+            fmt += neigh.struct_type
+        self.struct = StructField.get_struct(fmt)
+
+    def from_python(self, data):
+        pass
+
+    def to_python(self, data, index):
+        pass
