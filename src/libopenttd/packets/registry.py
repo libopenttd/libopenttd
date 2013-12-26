d59558f63c8ac3ec272ec85c2291af3e9e35cc40
Xaroth Brook <devnull@xaroth.nl>
2013-12-26 02:07:24 +0100

    -Add: Packet registry, to keep track of all known packets.

---
+++ src/libopenttd/packets/registry.py
@@ -0,0 +1,54 @@
+from collections import defaultdict, OrderedDict
+from libopenttd.utils import lru_cache
+
+from .enums import Protocol, Direction
+
+import warnings
+
+class PacketRegistry(object):
+    def __init__(self):
+        # Mapping of ProtocolType => Direction => PacketID => Packet classes.
+        # This way we have quick and easy access to the right packets.
+        self.all_packets = defaultdict(lambda: defaultdict(dict))
+
+    def register_packet(self, packet):
+        directions = packet.direction
+        if packet.direction & Direction.BOTH == Direction.BOTH:
+            directions = [Direction.SEND, Direction.RECV, Direction.BOTH]
+        for direction in directions:
+            if packet.pid in self.all_packets[packet.proto][direction]:
+                # We don't re-register packets for a specific PID
+                existing = self.all_packets[packet.proto][direction][packet.pid]
+                if existing == packet:
+                    continue
+                print packet
+                print existing
+                warnings.warn("Packet class '%s' collides with class '%s' for the direction '%s' and protocol '%s'. "
+                                "as a result, this packet will not register." % (packet._meta.name,
+                                existing._meta.name, Direction.get_name(direction), 
+                                Protocol.get_name(packet.proto)))
+                continue
+            self.all_packets[packet.proto][direction][packet.pid] = packet
+
+    #Optimize this method for speed
+    @lru_cache.lru_cache(maxsize=None)
+    def get_packets(self, protocol, direction, only_both = False):
+        packets = {}
+        if direction & Direction.BOTH == Direction.BOTH:
+            if only_both:
+                directions = [Direction.BOTH]
+            else:
+                directions = [Direction.SEND, Direction.RECV]
+        else:
+            directions = [direction]
+        for direction in directions:
+            packets.update(dict(self.all_packets[protocol][direction].items()))
+        return packets.values()
+
+    def get_registered_packet(self, packet):
+        return self.get_packet(packet.proto, packet.direction, packet.pid)
+
+    def get_packet(self, protocol, direction, pid):
+        return self.all_packets[protocol][direction].get(pid)
+
+registry = PacketRegistry()
