f458f271f2ab3feef08a1c19e65adc3a66690a50
Xaroth Brook <devnull@xaroth.nl>
2013-12-26 13:06:12 +0100

    -Add: import StructField into the main list as well.

--- src/libopenttd/packets/__init__.py
+++ src/libopenttd/packets/__init__.py
@@ -1,5 +1,5 @@
 from .base import PacketBase, FieldBase
 from .enums import Protocol, Direction
-from .fields import Field
+from .fields import Field, StructField
 from .packet import Packet
 from .registry import registry
