d06decdc32f06daace0b2a5234b5008179d27670
Xaroth Brook <devnull@xaroth.nl>
2013-12-26 13:36:01 +0100

    -Add: Exceptions module.

--- src/libopenttd/packets/__init__.py
+++ src/libopenttd/packets/__init__.py
@@ -1,5 +1,6 @@
 from .base import PacketBase, FieldBase
 from .enums import Protocol, Direction
+from .exceptions import InvalidReturnCount
 from .fields import Field, StructField, CharField, BooleanField, UByteField, ByteField, UInt8Field, \
     SByteField, Int8Field, UShortField, UInt16Field, SShortField, Int16Field, UIntField, ULongField, \
     UInt32Field, SIntField, LongField, Int32Field, ULongLongField, UInt64Field, SLongLongField, Int64Field
---
+++ src/libopenttd/packets/exceptions.py
@@ -0,0 +1,2 @@
+class InvalidReturnCount(Exception):
+    pass
