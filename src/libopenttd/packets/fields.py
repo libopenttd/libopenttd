095eabdb2e1528b21f5681ef18d31fad5f188857
Xaroth Brook <devnull@xaroth.nl>
2013-12-26 02:31:25 +0100

    -Add: some repr/str/unicode magic to the base Field.

--- src/libopenttd/packets/fields.py
+++ src/libopenttd/packets/fields.py
@@ -1,4 +1,15 @@
 from .base import PacketFieldBase
+from libopenttd.utils import six
 
 class Field(PacketFieldBase):
-    pass
+    def __repr__(self):
+        try:
+            utf = six.text_type(self)
+        except (UnicodeEncodeError, UnicodeDecodeError):
+            utf = '[Bad Unicode data]'
+        return '<%s: %s>'  % (self.__class__.__name__, utf)
+
+    def __str__(self):
+        if six.PY2 and hasattr(self, '__unicode__'):
+            return six.text_type(self).encode('utf-8')
+        return '%s field' % self.name
