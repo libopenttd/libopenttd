c58a99d1e253c3f0eebbf1a53d3a67f8f716b571
Xaroth Brook <devnull@xaroth.nl>
2013-12-26 02:09:17 +0100

    -Change: change up __init__.py with the new classes.

--- src/libopenttd/packets/__init__.py
+++ src/libopenttd/packets/__init__.py
@@ -1,2 +1,5 @@
-from .packet import Packet
+from .base import PacketBase, PacketFieldBase
+from .enums import Protocol, Direction
 from .fields import Field
+from .packet import Packet
+from .registry import registry
