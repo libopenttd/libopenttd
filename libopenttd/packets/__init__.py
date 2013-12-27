2c088093a081ae67d28d1d181e40a6d1a2b13029
Xaroth Brook <devnull@xaroth.nl>
2013-12-27 11:16:22 +0100

    -Change: order enum imports... and add some more enums.
    Mostly taken from libopenttdadmin2....

--- libopenttd/packets/__init__.py
+++ libopenttd/packets/__init__.py
@@ -1,5 +1,5 @@
 from .base import PacketBase, FieldBase
-from .enums import Protocol, Direction
+from .enums import Direction, Protocol
 from .exceptions import InvalidReturnCount, InvalidFieldName
 from .fields import Field, StringField, StructField, CharField, BooleanField, UByteField, ByteField, UInt8Field, \
     SByteField, Int8Field, UShortField, UInt16Field, SShortField, Int16Field, UIntField, ULongField, \
@@ -8,4 +8,5 @@ from .packet import Packet
 from .registry import registry
 
 import constants
+import enums
 import validators
