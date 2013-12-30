ed4a79c6607b4020fddae4fa585bcdd8d34d5b26
Xaroth Brook <devnull@xaroth.nl>
2013-12-31 00:49:27 +0100

    -Add: The remaining Admin packet descriptors.

--- libopenttd/admin/packets/recv.py
+++ libopenttd/admin/packets/recv.py
@@ -135,3 +135,49 @@ class CompanyStats(Packet):
         'plane':    packets.UInt16Field(ordering=4),
         'ship':     packets.UInt16Field(ordering=5),
         })
+
+class Chat(Packet):
+    pid = 119
+    action      = packets.UInt8Field(ordering=1)
+    dest_type   = packets.UInt8Field(ordering=2)
+    client_id   = packets.UInt32Field(ordering=3)
+    message     = packets.StringField(ordering=4)
+    data        = packets.UInt64Field(ordering=5)
+
+class Rcon(Packet):
+    pid = 120
+    colour      = packets.UInt16Field(ordering=1)
+    result      = packets.StringField(ordering=2)
+
+class Console(Packet):
+    pid = 121
+    origin      = packets.StringField(ordering=1)
+    message     = packets.StringField(ordering=2)
+
+class CmdNames(Packet):
+    pid = 122
+    commands    = packets.DictField(ordering=1, key=packets.UInt16Field(ordering=1),
+                                    value=packets.StringField(ordering=2),
+                                    is_next=packets.BooleanField(ordering=3, is_next=True))
+
+class CmdLogging(Packet):
+    pid = 123
+    client_id   = packets.UInt32Field(ordering=1)
+    company_id  = packets.UInt8Field(ordering=2)
+    command_id  = packets.UInt16Field(ordering=3)
+    params      = packets.UInt32Field(ordering=4, count=2)
+    tile        = packets.UInt32Field(ordering=5)
+    text        = packets.StringField(ordering=6)
+    frame       = packets.UInt32Field(ordering=7)
+
+class Gamescript(Packet):
+    pid = 124
+    data        = packets.JsonField(ordering=1)
+
+class RconEnd(Packet):
+    pid = 125
+    command     = packets.StringField(ordering=1)
+
+class Pong(Packet):
+    pid = 126
+    payload     = packets.UInt32Field(ordering=1)
