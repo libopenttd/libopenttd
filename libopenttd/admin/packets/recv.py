5be0b137ff65f7121999127b3bcd78c0abf7184e
Xaroth Brook <devnull@xaroth.nl>
2013-12-30 17:38:24 +0100

    -Add: Some more packets.

--- libopenttd/admin/packets/recv.py
+++ libopenttd/admin/packets/recv.py
@@ -73,3 +73,47 @@ class ClientError(Packet):
     pid = 112
     client_id   = packets.UInt32Field(ordering=1)
     errorcode   = packets.UInt8Field(ordering=2)
+
+class CompanyNew(Packet):
+    pid = 113
+    company_id  = packets.UInt8Field(ordering=1)
+
+class CompanyInfo(Packet):
+    pid = 114
+    company_id  = packets.UInt8Field(ordering=1)
+    name        = packets.StringField(ordering=2)
+    manager_name = packets.StringField(ordering=3)
+    colour      = packets.UInt8Field(ordering=4)
+    passworded  = packets.BooleanField(ordering=5)
+    startyear   = packets.UInt32Field(ordering=6)
+    is_ai       = packets.BooleanField(ordering=7)
+    bankrupcy_counter = packets.UInt8Field(ordering=8)
+    shareholder = packets.UInt8Field(ordering=9, count=4)
+
+class CompanyUpdate(Packet):
+    pid = 115
+    company_id  = packets.UInt8Field(ordering=1)
+    name        = packets.StringField(ordering=2)
+    manager_name = packets.StringField(ordering=3)
+    colour      = packets.UInt8Field(ordering=4)
+    passworded  = packets.BooleanField(ordering=5)
+    bankrupcy_counter = packets.UInt8Field(ordering=8)
+    shareholder = packets.UInt8Field(ordering=9, count=4)
+
+class CompanyRemove(Packet):
+    pid = 116
+    company_id  = packets.UInt8Field(ordering=1)
+    reason      = packets.UInt8Field(ordering=2)
+
+class CompanyEconomy(Packet):
+    pid = 117
+    company_id  = packets.UInt8Field(ordering=1)
+    money       = packets.Int64Field(ordering=2)
+    current_loan = packets.Int64Field(ordering=3)
+    income      = packets.Int64Field(ordering=4)
+    delivered   = packets.UInt16Field(ordering=5)
+    history     = packets.RepeatingField(ordering=6, count=2, fields = {
+        'value': packets.Int64Field(ordering=1),
+        'performance': packets.UInt16Field(ordering=2),
+        'delivered': packets.UInt16Field(ordering=3),
+        })
