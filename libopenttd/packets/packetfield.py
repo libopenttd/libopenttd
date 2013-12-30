8f9243d249594ef5d61193e99ceda18caebfc1e3
Xaroth Brook <devnull@xaroth.nl>
2013-12-30 16:48:26 +0100

    -Remove: PacketField is not implemented differently.

--- libopenttd/packets/packetfield.py
+++
@@ -1,7 +0,0 @@
-from .base import FieldBase, PacketBase
-from libopenttd.utils import six
-
-class PacketField(six.with_metaclass(PacketBase, FieldBase)):
-
-    class Meta:
-        force_virtual = True
