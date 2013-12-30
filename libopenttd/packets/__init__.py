c864822a49f8411e3054021f72e69ff429a8d50a
Xaroth Brook <devnull@xaroth.nl>
2013-12-30 02:40:11 +0100

    -Change: import the new fields and exceptions.

--- libopenttd/packets/__init__.py
+++ libopenttd/packets/__init__.py
@@ -1,10 +1,10 @@
 from .base import PacketBase, FieldBase
 from .enums import Direction, Protocol
-from .exceptions import InvalidReturnCount, InvalidFieldName
+from .exceptions import InvalidReturnCount, InvalidFieldName, InvalidFieldData, InvalidPacketLayout
 from .fields import Field, StringField, StructField, CharField, BooleanField, UByteField, ByteField, UInt8Field, \
     SByteField, Int8Field, UShortField, UInt16Field, SShortField, Int16Field, UIntField, ULongField, \
     UInt32Field, SIntField, LongField, Int32Field, ULongLongField, UInt64Field, SLongLongField, Int64Field, \
-    JsonField
+    JsonField, LoopingField, DictField, DateField
 from .packet import Packet
 from .registry import registry
 
