bc2b390141ffcce372874886987a0f1cef5b783f
Xaroth Brook <devnull@xaroth.nl>
2014-01-03 23:17:38 +0100

    -Change: set the admin protocol's default_version.

--- libopenttd/admin/packets/base.py
+++ libopenttd/admin/packets/base.py
@@ -4,3 +4,4 @@ class AdminPacket(packets.Packet):
     class Meta:
         protocol = packets.Protocol.ADMIN
         abstract = True
+        default_version = packets.constants.NETWORK_GAME_ADMIN_VERSION
