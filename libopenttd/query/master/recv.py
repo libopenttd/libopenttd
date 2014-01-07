33fa6db63e1251a3027022b8a46b1c076987ce69
Xaroth Brook <devnull@xaroth.nl>
2014-01-07 23:22:11 +0100

    -Add: Finish ServerList packet.
    This should conclude the Query protocol.

--- libopenttd/query/master/recv.py
+++ libopenttd/query/master/recv.py
@@ -15,7 +15,14 @@ class Registered(Packet):
 
 class ServerList(Packet):
     pid = 7
+    ip_type         = packets.IPAddrPrefixField(ordering=1)
+    addresses       = packets.RepeatingField(ordering=2,
+        count       = packets.UInt16Field(ordering=-1),
+        fields      = {
+            "ip"    : packets.IPAddrField(ordering=1),
+            "port"  : packets.UInt16Field(ordering=2),
+        })
 
 class SessionKey(Packet):
     pid  = 11
-    session_key = packets.UInt64Field()
+    session_key     = packets.UInt64Field()
